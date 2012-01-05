625f016385bf017c2728d68c679dca407d490987
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2012-01-05 11:39:42 -0500

    adicionado: informe de cartera

---
+++ Clases/Clases/bin/Release/src/Saldoporcobrar.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using ServiceStack.Common;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmSimple;
+
+namespace  SuperGym.Records  
+{
+ 	[Alias("SALDOPORCOBRAR")]
+	public partial class Saldoporcobrar{
+
+ 		public Saldoporcobrar(){}
+
+ 		[Alias("IDPERSONA")]
+		[PrimaryKey]
+		[Required]
+		public System.Int32 Idpersona { get; set;} 
+
+		[Alias("SALDO")]
+		[Required]
+		public System.Decimal Saldo { get; set;} 
+
+		[Alias("FECHA")]
+		[Required]
+		public System.DateTime Fecha { get; set;} 
+
+ 	}
+ }
